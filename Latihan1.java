ee24a21d63b84de8f109d6b6e1f6cc30312d5678
attahiya-rahma-habibila <attahiyarahma@gmail.com>
2025-05-21 13:54:36 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,13 @@
+package com.belajar;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        String nama = "atta";
+        String kelas = " X PPLG 2";
+        double B_badan = 48.00;
+        System.out.println("Nama saya : " +nama);
+        System.err.println("kelas : " +kelas);
+        System.err.print("Berat Badan : ");
+        System.out.println(B_badan);
+    }
+}
